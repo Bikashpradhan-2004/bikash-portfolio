17761d7bea07432b3a182630c9efa8b823679d43
Bikashpradhan-2004 <bkp96264@gmail.com>
2025-10-10 10:08:53 +0530

    feat: implement Home component layout with header, download button, social links, and stats

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,48 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
+import Social from "@/components/Social";
+import Photo from "@/components/Photo";
+import Stats from "@/components/Stats";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          <div className="text-center xl:text-left order-2 xl:order-none">
+            <span className="text-xl">Software Developer</span>
+            <h1 className="h1 text-3xl lg:text-5xl font-semibold my-6">
+              Hello I'm <br />{" "}
+              <span className="text-accent mt-2">Bikash Kumar Pradhan</span>
+            </h1>
+            <p className="max-w-xl mb-9 text-white/80 text-sm md:text-base">
+              I excel at crefting elegant experiences and I am proficient in
+              various programming languages and techologies.
+            </p>
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              <Button
+                variant="outline"
+                size="lg"
+                className="uppercase flex items-center gap-2 rounded-full"
+              >
+                <span>Download CV</span>
+                <FiDownload className="text-xl" />
+              </Button>
+              <div className="mb-8 xl:mb-0">
+                <Social
+                  containerStyles="flex gap-6"
+                  iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
+            <Photo />
+          </div>
+        </div>
+      </div>
+      <Stats />
+    </section>
+  );
 }

e5e2bd865a8a5e74896f1ed7024c07dd57f92500
Bikashpradhan-2004 <bkp96264@gmail.com>
2025-10-13 23:25:15 +0530

    refactor: improve home page structure and content

--- components/Stats/index.jsx
+++ components/Stats/index.jsx
@@ -2,33 +2,38 @@
 import CountUp from "react-countup";
 
 const stats = [
-  { number: 100, text: "Years of experience" },
-  { number: 200, text: "Projects completed" },
-  { number: 300, text: "Technologies mastered" },
-  { number: 400, text: "Code commits" },
+  { number: 8, text: "months of experience" },
+  { number: 13, text: "Technologies known" },
+  { number: 160, text: "Code commits" },
+  { number: 270, text: "Leetcode Problems" },
 ];
 
 const Stats = () => {
   return (
     <section>
       <div className="container mx-auto">
-        <div className="flex flex-wrap gap-4 max-w-[80vw] mx-auto xl-max-w-none">
+        <div className="flex flex-wrap gap-2 md:gap-4 max-w-[80vw] mx-auto xl:max-w-none">
           {stats.map((stat, index) => {
             return (
               <div
-                className="flex-1 flex gap-4 items-center justify-cente xl:justify-start"
                 key={index}
+                className="flex-1 flex gap-4 items-center justify-center xl:justify-start"
               >
-                <CountUp
-                  end={stat.number}
-                  duration={5}
-                  delay={2}
-                  className="text-3xl xl:text-6xl font-extrabold"
-                />
+                <div className="flex">
+                  <CountUp
+                    end={stat.number}
+                    duration={5}
+                    delay={2}
+                    className="text-2xl md:text-4xl xl:text-6xl font-extrabold"
+                  />
+                  <p className="text-xl md:text-2xl xl:text-4xl font-extrabold ml-1 my-auto">
+                    +
+                  </p>
+                </div>
                 <p
                   className={`${
                     stat.text.length < 15 ? "max-w-[100px]" : "max-w-[150px]"
-                  } leading-snug text-white/80`}
+                  } leading-snug text-white/80 text-sm lg:text-base`}
                 >
                   {stat.text}
                 </p>

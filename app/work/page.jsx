f7040561f90c9af931637ffcea0758c0736148b1
Bikashpradhan-2004 <bkp96264@gmail.com>
2025-10-14 14:47:38 +0530

    refactor: update content of work page

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -17,40 +17,87 @@ import WorkSliderBtns from "@/components/ui/WorkSliderBtns";
 const projects = [
   {
     num: "01",
-    category: "frontend",
+    category: "Portfolio",
     title: "project 1",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate magnam modi.",
-    stack: [{ name: "Html 5" }, { name: "Css 3" }, { name: "Javascript" }],
-    image: "",
-    live: "",
-    github: "",
+      "A modern portfolio showcasing my projects, skills, and experience as a passionate full-stack web developer.",
+    stack: [
+      { name: "Html 5" },
+      { name: "Tailwind CSS" },
+      { name: "Next.js" },
+      { name: "Javascript" },
+    ],
+    image: "/websides/Portfolio.png",
+    live: "https://www.bikashcodes.online/",
+    github: "https://github.com/Bikashpradhan-2004/bikash-portfolio",
   },
   {
     num: "02",
-    category: "frontend",
+    category: "EdubizNetworks",
     title: "project 2",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate magnam modi.",
-    stack: [{ name: "Html 5" }, { name: "Css 3" }, { name: "Javascript" }],
-    image: "",
-    live: "",
+      "EduBiz Networks empowers the education sector with franchising, consulting, marketing, and growth solutions, connecting educators, learners, and businesses for sustainable impact.",
+    stack: [
+      { name: "Html 5" },
+      { name: "Tailwind CSS" },
+      { name: "Next.js" },
+      { name: "Typescript" },
+    ],
+    image: "/websides/EdubizNetworks.png",
+    live: "https://edubiznetworks.com/",
     github: "",
   },
   {
     num: "03",
-    category: "frontend",
+    category: "fransell",
     title: "project 3",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate magnam modi.",
-    stack: [{ name: "Html 5" }, { name: "Css 3" }, { name: "Javascript" }],
-    image: "",
-    live: "",
+      "Fransell by EduBiz Networks helps education businesses expand through complete, end-to-end franchising solutions with strategic, operational, and marketing support",
+    stack: [
+      { name: "Html 5" },
+      { name: "Tailwind CSS" },
+      { name: "Next.js" },
+      { name: "Typescript" },
+    ],
+    image: "/websides/fransell.png",
+    live: "https://fransell.edubiznetworks.com/",
+    github: "",
+  },
+  {
+    num: "04",
+    category: "Talenza",
+    title: "project 4",
+    description:
+      "Talenza by EduBiz Networks lets students showcase talents via short videos, gain recognition, and be featured on our website and YouTube channel.",
+    stack: [
+      { name: "Html 5" },
+      { name: "Tailwind CSS" },
+      { name: "Next.js" },
+      { name: "Typescript" },
+    ],
+    image: "/websides/Talenza.png",
+    live: "https://talenza.netlify.app/",
+    github: "",
+  },
+  {
+    num: "05",
+    category: "Boostup",
+    title: "project 5",
+    description:
+      "BoostUp by EduBiz Networks offers smart marketing solutions for educational brands, enhancing growth, reducing costs, and increasing visibility for schools and institutes.",
+    stack: [
+      { name: "Html 5" },
+      { name: "Tailwind CSS" },
+      { name: "Next.js" },
+      { name: "Typescript" },
+    ],
+    image: "/websides/Boostup.png",
+    live: "https://boostup.edubiznetworks.com/",
     github: "",
   },
 ];
 
-const work = () => {
+const Work = () => {
   const [project, setProject] = useState(projects[0]);
 
   const handleSlideChange = (swiper) => {
@@ -68,16 +115,16 @@ const work = () => {
         <div className="flex flex-col xl:flex-row xl:gap-[30px]">
           <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
             <div className="flex flex-col gap-[30px] h-[50%]">
-              <div className="text-8xl leading-none font-extrabold text-transparent text-outline">
+              <div className="text-6xl md:text-8xl leading-none font-extrabold text-transparent text-outline">
                 {project.num}
               </div>
               <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize">
-                {project.category} project
+                {project.category} website
               </h2>
               <p className="text-white/60">{project.description}</p>
-              <ul className="flex gap-4">
+              <ul className="flex gap-2 md:gap-4">
                 {project.stack.map((item, index) => (
-                  <li key={index} className="text-xl text-accent">
+                  <li key={index} className="md:text-xl text-accent">
                     {item.name}
                     {index !== project.stack.length - 1 && ","}
                   </li>
@@ -85,7 +132,7 @@ const work = () => {
               </ul>
               <div className="border border-white/20"></div>
               <div className="flex items-center gap-4">
-                <Link href={project.live}>
+                <Link href={project.live} target="_blank">
                   <TooltipProvider delayDuration={100}>
                     <Tooltip>
                       <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
@@ -97,18 +144,20 @@ const work = () => {
                     </Tooltip>
                   </TooltipProvider>
                 </Link>
-                <Link href={project.github}>
-                  <TooltipProvider delayDuration={100}>
-                    <Tooltip>
-                      <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
-                        <BsGithub className="text-white text-3xl group-hover:text-accent" />
-                      </TooltipTrigger>
-                      <TooltipContent>
-                        <p>Github repository</p>
-                      </TooltipContent>
-                    </Tooltip>
-                  </TooltipProvider>
-                </Link>
+                {project.github && (
+                  <Link href={project.github} target="_blank">
+                    <TooltipProvider delayDuration={100}>
+                      <Tooltip>
+                        <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                          <BsGithub className="text-white text-3xl group-hover:text-accent" />
+                        </TooltipTrigger>
+                        <TooltipContent>
+                          <p>Github repository</p>
+                        </TooltipContent>
+                      </Tooltip>
+                    </TooltipProvider>
+                  </Link>
+                )}
               </div>
             </div>
           </div>
@@ -129,7 +178,7 @@ const work = () => {
                           src={project.image}
                           fill
                           className="object-cover"
-                          alt=""
+                          alt={project.title}
                         />
                       </div>
                     </div>
@@ -147,4 +196,5 @@ const work = () => {
     </motion.section>
   );
 };
-export default work;
+
+export default Work;

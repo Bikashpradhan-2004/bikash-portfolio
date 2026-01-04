4fd40de7b105bf3605e82ee935ba96d1c895c918
Bikashpradhan-2004 <bkp96264@gmail.com>
2026-01-05 01:35:27 +0530

    refactor: update project details and add Attendox project to the portfolio

--- constants/projects.js
+++ constants/projects.js
@@ -1,8 +1,25 @@
 export const PROJECTS = [
   {
     num: "01",
-    category: "Portfolio",
+    category: "Attendox",
     title: "project 1",
+    description:
+      "An attendance management system designed to manage student records, track daily and monthly attendance, and present clear visual insights through dashboards, while supporting secure access and easy PDF report export for institutional use.",
+    stack: [
+      { name: "Next.js" },
+      { name: "Javascript" },
+      { name: "Supabase"},
+      { name: "Drizzle ORM"},
+      { name: "Kinde auth"}
+    ],
+    image: "/websides/Attendox.png",
+    live: "https://attendox.bikashcodes.online/",
+    github: "https://github.com/Bikashpradhan-2004/Attendox",
+  },
+  {
+    num: "02",
+    category: "Portfolio",
+    title: "project 2",
     description:
       "A modern portfolio showcasing my projects, skills, and experience as a passionate full-stack web developer.",
     stack: [
@@ -16,9 +33,9 @@ export const PROJECTS = [
     github: "https://github.com/Bikashpradhan-2004/bikash-portfolio",
   },
   {
-    num: "02",
+    num: "03",
     category: "EdubizNetworks",
-    title: "project 2",
+    title: "project 3",
     description:
       "EduBiz Networks empowers the education sector with franchising, consulting, marketing, and growth solutions, connecting educators, learners, and businesses for sustainable impact.",
     stack: [
@@ -32,9 +49,9 @@ export const PROJECTS = [
     github: "",
   },
   {
-    num: "03",
+    num: "04",
     category: "fransell",
-    title: "project 3",
+    title: "project 4",
     description:
       "Fransell by EduBiz Networks helps education businesses expand through complete, end-to-end franchising solutions with strategic, operational, and marketing support",
     stack: [
@@ -48,9 +65,9 @@ export const PROJECTS = [
     github: "",
   },
   {
-    num: "04",
+    num: "05",
     category: "Talenza",
-    title: "project 4",
+    title: "project 5",
     description:
       "Talenza by EduBiz Networks lets students showcase talents via short videos, gain recognition, and be featured on our website and YouTube channel.",
     stack: [
@@ -64,9 +81,9 @@ export const PROJECTS = [
     github: "",
   },
   {
-    num: "05",
+    num: "06",
     category: "Boostup",
-    title: "project 5",
+    title: "project 6",
     description:
       "BoostUp by EduBiz Networks offers smart marketing solutions for educational brands, enhancing growth, reducing costs, and increasing visibility for schools and institutes.",
     stack: [

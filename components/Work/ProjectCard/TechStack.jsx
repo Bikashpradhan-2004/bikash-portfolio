3e420b9e78541c6692407173635cef3468bce53e
Bikashpradhan-2004 <bkp96264@gmail.com>
2026-01-05 01:34:15 +0530

    refactor: adjust text size for TechStack component list items feat: add Attendox image to public web assets

--- components/Work/ProjectCard/TechStack.jsx
+++ components/Work/ProjectCard/TechStack.jsx
@@ -2,7 +2,7 @@ const TechStack = ({ stack }) => {
   return (
     <ul className="flex gap-2 md:gap-4">
       {stack.map((item, index) => (
-        <li key={index} className="md:text-xl text-accent">
+        <li key={index} className="text-xs md:text-xl text-accent">
           {item.name}
           {index !== stack.length - 1 && ","}
         </li>
